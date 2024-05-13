3e262f6ca6103bb9d7dfd84410725e37cc459055
Christiaan Meijer <c.meijer@esciencecenter.nl>
2024-05-13 21:36:37 +0200

    remove unexisting techniques and add fragments

---
+++ create_techniques_data.py
@@ -0,0 +1,40 @@
+import json
+import itertools
+
+numerals = [
+    "first", "second", "third", "fourth", "fifth",
+    "sixth", "seventh", "eighth", "ninth", "tenth",
+    "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth",
+    "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth",
+    "twenty-first", "twenty-second", "twenty-third", "twenty-fourth", "twenty-fifth",
+    "twenty-sixth", "twenty-seventh", "twenty-eighth", "twenty-ninth", "thirtieth",
+    "thirty-first", "thirty-second", "thirty-third", "thirty-fourth", "thirty-fifth",
+    "thirty-sixth", "thirty-seventh", "thirty-eighth", "thirty-ninth", "fortieth",
+    "forty-first", "forty-second", "forty-third", "forty-fourth", "forty-fifth",
+    "forty-sixth", "forty-seventh", "forty-eighth", "forty-ninth", "fiftieth"
+]
+
+strike_types = ['upward', 'downward', 'sideward', 'forward', 'combination', 'extension', ]
+fragment = ['fragment']
+
+
+def main():
+    yellow_techniques = expand(numerals[:2], strike_types) + expand(numerals[:5], fragment)
+    orange_techniques = expand(numerals[2:4], strike_types) + expand(numerals[5:10], fragment)
+    green_techniques = expand(numerals[4:6], strike_types[:3] + strike_types[5:6]) + \
+                       expand(numerals[4:5], strike_types[5:6]) + expand(numerals[10:15], fragment)
+    blue_techniques = expand(numerals[6:8], strike_types[:3]) + expand(numerals[15:25], fragment)
+
+    all_techniques = sorted(yellow_techniques + orange_techniques + green_techniques + blue_techniques,
+                            key=lambda x: (strike_types+fragment).index(x.split(' ')[1]))
+
+    with open('techniques.json', 'w') as f:
+        json.dump(all_techniques, f, indent=2)
+
+
+def expand(zero_to_two, types_):
+    return [' '.join(t) for t in list(itertools.product(zero_to_two, types_))]
+
+
+if __name__ == "__main__":
+    main()
